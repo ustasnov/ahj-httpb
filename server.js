05f7a605787222ed02ac3d4bf8c482ddaf9745b5
ustasnov <ustasnov@gmail.com>
2024-03-10 20:04:46 +0700

    Changes for server

--- server.js
+++ server.js
@@ -5,12 +5,42 @@ const uuid = require('uuid');
 
 const app = new Koa();
 
+const tickets = [];
+
 app.use(koaBody({ urlencoded: true }));
+
+app.use((ctx, next) => {
+  if (ctx.request.method !== "OPTIONS") {
+    next();
+
+    return;
+  }
+
+  ctx.response.set('Access-Control-Allow-Origin', '*');
+  ctx.response.set('Access-Control-Allow-Methods', 'DELETE, PUT, PATCH, GET, POST');
+
+  ctx.response.status = 204;
+});
 
 app.use((ctx, next) => {
   console.log(ctx.request.body);
 
+  const { method } = ctx.request.querystring;
+
   ctx.response.set('Access-Control-Allow-Origin', '*');
+
+  switch (method) {
+    case 'allTickets':
+      ctx.response.body = tickets;
+      return;
+        // TODO: обработка остальных методов
+    case 'createTicket':
+      return;
+    default:
+      ctx.response.status = 404;
+      return;
+    }
+
 
   ctx.response.body = 'server response';
 
