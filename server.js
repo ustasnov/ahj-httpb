5a70ca9903916d87d1d7b557d989b6f5bce4700b
ustasnov <ustasnov@gmail.com>
2024-03-10 10:09:37 +0700

    Changes for server code

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const http = require('http');
 const Koa = require('koa');
 const koaBody = require('koa-body');
+const uuid = require('uuid');
 
 const app = new Koa();
 
-app.use(koaBody({urlencoded: true}));
+app.use(koaBody({ urlencoded: true }));
 
 app.use((ctx, next) => {
   console.log(ctx.request.body);
+
+  ctx.response.set('Access-Control-Allow-Origin', '*');
 
   ctx.response.body = 'server response';
 
@@ -23,9 +26,9 @@ const server = http.createServer(app.callback());
 const port = 7070;
 // слушаем определённый порт
 server.listen(port, (err) => {
-    if (err) {
-      console.log('Error occured:', err);
-      return; 
-    }
-    console.log(`server is listening on ${port}`);
+  if (err) {
+    console.log('Error occured:', err);
+    return;
+  }
+  console.log(`server is listening on ${port}`);
 });
