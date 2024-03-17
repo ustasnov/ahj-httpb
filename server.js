2686e83fdf2ced385e90c367b50120dd657c1adb
ustasnov <ustasnov@gmail.com>
2024-03-17 22:35:52 +0700

    Final changes

--- server.js
+++ server.js
@@ -1,38 +1,61 @@
-const http = require('http');
-const Koa = require('koa');
-const koaBody = require('koa-body');
-const uuid = require('uuid');
-const querystring = require('node:querystring'); 
+/* eslint-env node */
+/* eslint no-unused-vars: "off" */
+
+const http = require("http");
+const Koa = require("koa");
+const koaBody = require("koa-body");
+const uuid = require("uuid");
+const querystring = require("node:querystring");
+const fs = require("fs");
 
 const app = new Koa();
 
-const tickets = [
-  {
-    id: uuid.v4(),
-    name: "Задание 1",
-    description: "Полное описание задания 1",
-    status: 1,
-    created: "10.04.24 10:00",
-  },
-  {
-    id: uuid.v4(),
-    name: "Задание 2",
-    description: "Полное описание задания 2",
-    status: 0,
-    created: "11.04.24 12:00",
-  },
-  {
-    id: uuid.v4(),
-    name: "Задание 4",
-    description: "Полное описание задания 4",
-    status: 0,
-    created: "13.04.24 14:00",
-  },
-];
+let tickets = [];
 
-app.use(koaBody({ 
-  urlencoded: true
-}));
+const dataFileName = "./public/data.json";
+
+function saveData() {
+  const stringData = JSON.stringify(tickets);
+  fs.writeFileSync(
+    dataFileName,
+    stringData,
+    (err) => {
+      if (err) {
+        throw new Error(`Error occured: ${err}`);
+      }
+    },
+    { encoding: "utf8", flag: "w" }
+  );
+  console.log("data save to file");
+}
+
+function loadData() {
+  const stringData = fs.readFileSync(dataFileName, {
+    encoding: "utf8",
+    flag: "r",
+  });
+  tickets = JSON.parse(stringData);
+  console.log("data load from file");
+}
+
+function getCurrentDateString() {
+  const options = {
+    year: "2-digit",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+  };
+  const currentDate = new Date();
+  return currentDate.toLocaleString("ru", options).replace(",", "");
+}
+
+app.use(
+  koaBody({
+    urlencoded: true,
+    multipart: true,
+  })
+);
 
 app.use((ctx, next) => {
   if (ctx.request.method !== "OPTIONS") {
@@ -41,65 +64,126 @@ app.use((ctx, next) => {
     return;
   }
 
-  ctx.response.set('Access-Control-Allow-Origin', '*');
-  ctx.response.set('Access-Control-Allow-Methods', 'DELETE, PUT, PATCH, GET, POST');
-  ctx.response.set('Access-Control-Allow-Headers', "Content-Type");
+  ctx.response.set("Access-Control-Allow-Origin", "*");
+  ctx.response.set(
+    "Access-Control-Allow-Methods",
+    "DELETE, PUT, PATCH, GET, POST"
+  );
+  ctx.response.set("Access-Control-Allow-Headers", "Content-Type");
+  ctx.response.set("Content-Type", "application/json; charset=utf-8");
 
   ctx.response.status = 204;
 });
 
 app.use((ctx, next) => {
+  if (ctx.request.method !== "DELETE") {
+    next();
 
-  //console.log(ctx.request.body);
-  console.log(ctx.request.querystring);
+    return;
+  }
 
-  ctx.response.set('Access-Control-Allow-Origin', '*');
-  
+  ctx.response.set("Access-Control-Allow-Origin", "*");
 
   const queryParams = querystring.parse(ctx.request.querystring);
+  console.log(ctx.request.querystring);
+  if (queryParams.method == "deleteTicket") {
+    const { id } = ctx.request.query;
+    const deletedTicket = Array.from(tickets).find((el) => el.id == id);
+    let deleteId = id;
+    if (deletedTicket) {
+      deleteId = deletedTicket.id;
+      tickets.splice(tickets.indexOf(deletedTicket), 1);
+      saveData();
+    } else {
+      console.log(`ticket for delete with id = ${id} not found`);
+    }
+    ctx.response.body = JSON.stringify({ result: 0, data: deleteId });
+  } else {
+    ctx.response.status = 404;
+    return;
+  }
+});
+
+app.use((ctx, next) => {
+  ctx.response.set("Access-Control-Allow-Origin", "*");
+
+  const queryParams = querystring.parse(ctx.request.querystring);
+  console.log(ctx.request.querystring);
+  console.log(queryParams);
+  let ticket = null;
+  let id = "",
+    name = "",
+    description = "",
+    status = 0;
   switch (queryParams.method) {
-    case 'allTickets':
-      ctx.response.set('Content-Type', "application/json; charset=utf-8");
-      ctx.response.body = JSON.stringify(tickets);
+    case "allTickets":
+      loadData();
+      ctx.response.body = JSON.stringify({ result: 0, data: tickets });
       return;
-    // TODO: обработка остальных методов
-    case 'ticketById':
-      ctx.response.set('Content-Type', "application/json; charset=utf-8");
-      const ticket = Array.from(tickets).find((el) => {
-        return el.id == queryParams.id;
-      });
+    case "ticketById":
+      ticket = Array.from(tickets).find((e) => e.id == queryParams.id);
       if (ticket) {
-        ctx.response.body = JSON.stringify(ticket);  
+        ctx.response.body = JSON.stringify({ result: 0, data: ticket });
       } else {
-        ctx.response.status = 404;
+        console.log(`ticket with id = ${queryParams.id} not found`);
+        ctx.response.body = JSON.stringify({ result: 1, data: {} });
       }
       return;
-    case 'createTicket':
-      console.log(ctx.request.body);
-      ctx.response.set('Content-Type', "application/json; charset=utf-8");
-      const {name, description} = ctx.request.body;
-      console.log(`name: ${ctx.request.body.name}, description: ${ctx.request.body.description}`);
-      ctx.response.body = "{}";
+    case "createTicket":
+      console.log(
+        `name: ${ctx.request.body.name}, description: ${ctx.request.body.description}`
+      );
+      ticket = {
+        id: uuid.v4(),
+        name: ctx.request.body.name,
+        description: ctx.request.body.description,
+        status: 0,
+        created: getCurrentDateString(),
+      };
+      tickets.push(ticket);
+      saveData();
+      ctx.response.body = JSON.stringify({ result: 0, data: ticket });
+      return;
+    case "updateTicket":
+      ({ id, name, description, status } = ctx.request.body);
+      console.log(
+        `name: ${ctx.request.body.name}, description: ${ctx.request.body.description}`
+      );
+      ticket = Array.from(tickets).find((el) => el.id == id);
+      if (ticket) {
+        ticket.name = name;
+        ticket.description = description;
+        ticket.status = status;
+        saveData();
+        ctx.response.body = JSON.stringify({ result: 0, data: ticket });
+      } else {
+        console.log(`ticket with id = ${id} not found`);
+        ctx.response.body = JSON.stringify({ result: 1, data: {} });
+      }
+      return;
+    case "toggleTicketStatus":
+      ticket = Array.from(tickets).find((el) => el.id == ctx.request.body.id);
+      if (ticket) {
+        ticket.status = ticket.status == 0 ? 1 : 0;
+        saveData();
+        ctx.response.body = JSON.stringify({ result: 0, data: ticket });
+      } else {
+        console.log(`ticket with id = ${ctx.request.body.id} not found`);
+        ctx.response.body = JSON.stringify({ result: 1, data: {} });
+      }
       return;
     default:
       ctx.response.status = 404;
       return;
   }
-
-  next();
-});
-
-app.use((ctx) => {
-  console.log('I am a second middleware');
 });
 
 const server = http.createServer(app.callback());
 
 const port = 7070;
-// слушаем определённый порт
 server.listen(port, (err) => {
   if (err) {
-    console.log('Error occured:', err);
+    console.log("Error occured:", err);
     return;
   }
   console.log(`server is listening on ${port}`);
